f8f41514a9dfa76d4ef3ff1bf6ebf057c65aac1e
krisnawahyudii <krisna.wahyudi@bengkuluprov.go.id>
2025-04-01 21:41:52 +0700

    frat: login, logout auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 
-// user
-Route::controller(UserController::class)->group(function () {
-    Route::get('user', 'index');
-    Route::get('user/{id}', 'show');
-    Route::delete('user/{id}', 'destroy');
-    Route::post('user', 'store');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // user
+    Route::controller(UserController::class)->group(function () {
+        Route::get('user', 'index');
+        Route::get('user/{id}', 'show');
+        Route::delete('user/{id}', 'destroy');
+        Route::post('user', 'store');
+    });
 });

fecfb8b735967bc6ec60a9914de4d00843ffeef9
krisnawahyudii <krisna.wahyudi@bengkuluprov.go.id>
2025-04-01 20:32:48 +0700

    feat: CRUD Tabel User

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// user
+Route::controller(UserController::class)->group(function () {
+    Route::get('user', 'index');
+    Route::get('user/{id}', 'show');
+    Route::delete('user/{id}', 'destroy');
+    Route::post('user', 'store');
+});
